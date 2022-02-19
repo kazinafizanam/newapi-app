681e0744847d516b49bcf6194c98be96fa42c61d
Kazi Nafiz Anam <99716386+kazinafizanam@users.noreply.github.com>
2022-02-19 16:06:42 +0600

    added code

--- zoomapi+app/src/App.js
+++ zoomapi+app/src/App.js
@@ -1,25 +1,27 @@
-import logo from "./logo.svg";
+
 import "./App.css";
 import Zoom from "./Zoom";
 import { useState } from "react";
 
 function App() {
-  const [joinMeeting, setJoinMeeting] = useState(false);
-  return (
-    <div className="App">
-      {joinMeeting ? (
-        <Zoom />
-      ) : (
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <button style={{border: '1px solid #fff'}} onClick={() => setJoinMeeting(true)}>Join Meeting</button>
-        </header>
-      )}
-    </div>
-  );
+    const [joinMeeting, setJoinMeeting] = useState(false);
+    return (
+        <div className="App">
+            {joinMeeting ? (
+                <Zoom />
+            ) : (
+                <>
+                    <h1>Zoom meeting</h1>
+                    <button className="btn btn-zoom"
+                        style={{ border: "1px solid #fff" }}
+                        onClick={() => setJoinMeeting(true)}
+                    >
+                        Join Meeting
+                    </button>
+                </>
+            )}
+        </div>
+    );
 }
 
 export default App;
